95180f2b3e210d9539d03ab7e82d2bd26f60b8ca
sudarshan <sdevkota007@gmail.com>
2018-11-27 02:00:25 -0500

    camera system somehow working

--- simulate.py
+++ simulate.py
@@ -6,11 +6,58 @@ import numpy
 import pyrr
 from pyrr import matrix44, vector3
 import TextureLoader
-import math
-from OpenGLContext.scenegraph.basenodes import Sphere
-
+from Camera import Camera
 from PIL import Image
 from ObjLoader import *
+
+
+
+cam = Camera()
+keys = [False] * 1024
+lastX, lastY = 640, 360
+first_mouse = True
+
+
+
+def key_callback(window, key, scancode, action, mode):
+    if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
+        glfw.set_window_should_close(window, True)
+
+    if key >= 0 and key < 1024:
+        if action == glfw.PRESS:
+            keys[key] = True
+        elif action == glfw.RELEASE:
+            keys[key] = False
+
+
+def do_movement():
+    if keys[glfw.KEY_W]:
+        cam.process_keyboard("FORWARD", 0.05)
+    if keys[glfw.KEY_S]:
+        cam.process_keyboard("BACKWARD", 0.05)
+    if keys[glfw.KEY_A]:
+        cam.process_keyboard("LEFT", 0.05)
+    if keys[glfw.KEY_D]:
+        cam.process_keyboard("RIGHT", 0.05)
+
+
+def mouse_callback(window, xpos, ypos):
+    global first_mouse, lastX, lastY
+
+    if first_mouse:
+        lastX = xpos
+        lastY = ypos
+        first_mouse = False
+
+    xoffset = xpos - lastX
+    yoffset = lastY - ypos
+
+    lastX = xpos
+    lastY = ypos
+
+    cam.process_mouse_movement(xoffset, yoffset)
+
+
 
 def window_resize(window, width, height):
     glViewport(0, 0, width, height)
@@ -21,7 +68,8 @@ def main():
     if not glfw.init():
         return
 
-    w_width, w_height = 800, 600
+    w_width, w_height = 1280, 720
+    aspect_ratio = w_width / w_height
 
 
     window = glfw.create_window(w_width, w_height, "My OpenGL window", None, None)
@@ -32,6 +80,8 @@ def main():
 
     glfw.make_context_current(window)
     glfw.set_window_size_callback(window, window_resize)
+    glfw.set_key_callback(window, key_callback)
+    glfw.set_cursor_pos_callback(window, mouse_callback)
 
     obj = ObjLoader()
     obj.load_model("objects/sphere.obj")
@@ -64,13 +114,13 @@ def main():
     glEnable(GL_TEXTURE_2D)
     glUseProgram(shader)
 
-    glClearColor(0.0, 0.4, 0.5, 1.0)
+    glClearColor(0.0, 0.0, 0.0, 1.0)
     glEnable(GL_DEPTH_TEST)
 
 
 
-    view = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, -10.0, -60.0]))
-    projection = pyrr.matrix44.create_perspective_projection_matrix(65.0, w_width / w_height, 0.1, 100.0)
+    # view = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, -10.0, -60.0]))
+    projection = pyrr.matrix44.create_perspective_projection_matrix(65.0, w_width / w_height, 0.1, 1000.0)
     scale = pyrr.matrix44.create_from_scale(pyrr.Vector3([0.1, 0.1, 0.1]))
     # # ---------------create normalMatrix-----------------
     # modelView = numpy.matmul(view, model)
@@ -83,8 +133,9 @@ def main():
     model_loc = glGetUniformLocation(shader, "model")
     normal_loc = glGetUniformLocation(shader, "normalMatrix")
     scale_loc = glGetUniformLocation(shader, "scale")
+    scale_down_loc = glGetUniformLocation(shader, "scale_down")
 
-    glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
+
     glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
     glUniformMatrix4fv(scale_loc, 1, GL_FALSE, scale)
 
@@ -113,11 +164,20 @@ def main():
     while not glfw.window_should_close(window):
         glfw.poll_events()
 
+        do_movement()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         time = glfw.get_time()
 
+        view = cam.get_view_matrix()
+        glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
+
+
         #************************************SUN****************************************
         glBindTexture(GL_TEXTURE_2D, sun_tex)
+        # scale down planet
+        scale_down = matrix44.create_from_scale(pyrr.Vector3([1.0, 1.0, 1.0]))
+        glUniformMatrix4fv(scale_down_loc, 1, GL_FALSE, scale_down)
+        # rotation
         rotation_speed = time * 0.002
         model_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 1.0, 0.0]))
         rot_x = matrix44.create_from_y_rotation(rotation_speed)
@@ -139,8 +199,11 @@ def main():
         #********************************EARTH******************************************
         glBindTexture(GL_TEXTURE_2D, earth_tex)
 
-        revolution_speed = time * 0.8
-        rotation_speed = time * 2
+        revolution_speed = time * 0.1
+        rotation_speed = time * 1
+        # scale down planet
+        scale_down = matrix44.create_from_scale(pyrr.Vector3([0.1, 0.1, 0.1]))
+        glUniformMatrix4fv(scale_down_loc, 1, GL_FALSE, scale_down)
 
         # translation
         model = matrix44.create_from_translation(pyrr.Vector3([10.0, 1.0, 0.0]))
